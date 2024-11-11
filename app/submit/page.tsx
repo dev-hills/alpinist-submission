2d7db64412864d17a3f903d249123d81c5ec71a1
Emujede Hilary <104493382+dev-hills@users.noreply.github.com>
2024-11-11 11:53:26 +0100

    Update submission deadline

--- app/submit/page.tsx
+++ app/submit/page.tsx
@@ -43,7 +43,7 @@ const Submit = () => {
   });
 
   useEffect(() => {
-    const targetDate = new Date("2024-11-11T12:00:00");
+    const targetDate = new Date("2024-11-11T23:59:00");
     const interval = setInterval(() => {
       const now = new Date().getTime();
       const distance = targetDate.getTime() - now;
